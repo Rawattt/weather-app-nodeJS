d54a092ec18545a0ce3dac1b87178a128bb1b0b1
Shubham Rawat <rawatrajput7@gmail.com>
2020-12-01 23:18:48 +0530

    Added current location

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,24 +1,23 @@
-const request = require('request')
+const request = require('request');
+const forecastKey = require('./keys');
 
 const forecast = (long, lat, callback) => {
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&%20&appid=a6dc86ce0b34804aaa8b528eb97af8b6&units=metric`
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&%20&appid=${forecastKey}&units=metric`;
 
-    request({url, json:true}, (err,{body}) => {
-        if(err){
-                callback('Unable to connect to weather service!', undefined)
-            } else if(body.message){
-                callback('Unable to find location. Try again', undefined)
-            }
-            else{
-                callback(undefined, {
-                    description:body.daily[0].weather[0].description,
-                    temperature:body.current.temp
-                })
-            }
-    })
-
-}
+  request({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.message) {
+      callback('Unable to find location. Try again', undefined);
+    } else {
+      callback(undefined, {
+        description: body.daily[0].weather[0].description,
+        temperature: body.current.temp
+      });
+    }
+  });
+};
 
 module.exports = {
-    forecast:forecast
-}
+  forecast: forecast
+};
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,24 +1,26 @@
-const request = require('request')
+const request = require('request');
+const geocodeKey = require('./keys');
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoicjR3NHQiLCJhIjoiY2tib212MmlyMjJzMzMxbXkzanN1a3VsbiJ9.bJVEJBX4mmJMZLe0CCauvQ`
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${geocodeKey}`;
 
-    request({url, json:true}, (err, {body}) => {
-        if (err) {
-            callback('UnableS to connect to the location service', undefined)          
-        } else if(body.features.length === 0){
-            callback('Unable to find location. Try another search', undefined)
-        }
-        else{
-            callback(undefined, {
-                longitude:body.features[0].center[0],
-                latitude:body.features[0].center[1],
-                location:body.features[0].place_name
-            })
-        }
-    })
-}
+  request({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback('UnableS to connect to the location service', undefined);
+    } else if (body.features.length === 0) {
+      callback('Unable to find location. Try another search', undefined);
+    } else {
+      callback(undefined, {
+        longitude: body.features[0].center[0],
+        latitude: body.features[0].center[1],
+        location: body.features[0].place_name
+      });
+    }
+  });
+};
 
 module.exports = {
-    geocode:geocode,
-}
+  geocode: geocode
+};
